1ed3fe0058bbc15742afe94ccaa8fd4631e9b54a
满脑子都是 <8840151+hbanxian@user.noreply.gitee.com>
2022-04-19 12:48:21 +0800

    perf(settle): build router about settle

--- src/router/index.ts
+++ src/router/index.ts
@@ -69,7 +69,7 @@ const routes: RouteRecordRaw[] = [
         component: () => import('@/views/main/coupon/Coupon.vue')
       },
       {
-        path: 'settle',
+        path: '/settle',
         name: 'Settle',
         component: () => import('@/views/main/settle/Settle.vue')
       }
